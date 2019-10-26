b204f502e8e4ce1af864aa6ddf4e4c3502162d40
Ekaterina <milady_95@bk.ru>
2019-10-26 19:33:40 +1000

    Контрольная проверка #1

--- js/basket.js
+++ js/basket.js
@@ -1,23 +1,28 @@
-var btnBuy = document.querySelector(".button-buy");
+var buttonBuy = document.querySelectorAll(".button-buy"), index, buttom;
 
 var popapBasket = document.querySelector(".popap-basket");
+
 var closeBasket = popapBasket.querySelector(".button-close");
 
-btnBuy.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    popapBasket.classList.add("popap-show");
-});
+for (index = 0; index < buttonBuy.length; index++) {
+    button = buttonBuy[index];
+    button.addEventListener('click', function (event) {
+        console.log('click');
+        event.preventDefault();
+        popapBasket.classList.add("popap-basket-show");
+    });
+}
 
 closeBasket.addEventListener("click", function(evt) {
     evt.preventDefault();
-    popapBasket.classList.remove("popap-show");
+    popapBasket.classList.remove("popap-basket-show");
 });
 
 window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
         evt.preventDefault();
-        if (popapBasket.classList.contains("popap-show")) {
-            popapBasket.classList.remove("popap-show");
+        if (popapBasket.classList.contains("popap-basket-show")) {
+            popapBasket.classList.remove("popap-basket-show");
         }
     }
 });
--- js/map.js
+++ js/map.js
@@ -1,16 +1,17 @@
-var linkMap = document.querySelector(".open-map");
+var linkMap = document.querySelector(".index-map");
 
 var popapMap = document.querySelector(".popap-map");
+
 var closeMap = popapMap.querySelector(".button-close");
 
 linkMap.addEventListener("click", function(evt) {
     evt.preventDefault();
-    popapMap.classList.add("popap-show");
+    popapMap.classList.add("popap-map-show");
 });
 
 closeMap.addEventListener("click", function(evt) {
     evt.preventDefault();
-    popapMap.classList.remove("popap-show");
+    popapMap.classList.remove("popap-map-show");
 });
 
 window.addEventListener("keydown", function (evt) {
