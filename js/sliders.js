ae1ede1e159194f0d8f9b0272bd2ecb181c89397
Ekaterina <milady_95@bk.ru>
2019-10-28 22:53:09 +1000

    Исправления (ч. 3)

---
+++ js/sliders.js
@@ -0,0 +1,24 @@
+// var slideIndex = 0;
+// showSlides(slideIndex);
+// function plusSlides (n) {
+// 	showSlides(slideIndex += n);
+// }
+
+// function showSlides (n) {
+// 	var i;
+// 	var slides = document.getElementsByClassName("mySlides");
+
+// 	if (n > slides.length) {
+// 		slideIndex = 1;
+// 	}
+
+// 	if (n < 1) {
+// 		slideIndex = slides.length;
+// 	}
+
+// 	for (var i = 0; i < slides.length; i++) {
+// 		slides[i].style.display = "none";
+// 	}
+
+// 	slides[slideIndex=0].style.display = "block";
+// }
