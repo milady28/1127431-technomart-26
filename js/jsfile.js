5097e27d6e2122b052078a2b80e34cd43b80353d
milady28 <53426214+milady28@users.noreply.github.com>
2019-11-01 15:06:08 +1000

    Проверка #1 (исп.3)

--- js/jsfile.js
+++ js/jsfile.js
@@ -1,20 +1,44 @@
+var buttonBuy = document.querySelectorAll(".button-buy"), index, buttom;
+var popapBasket = document.querySelector(".popap-basket");
+var closeBasket = popapBasket.querySelector(".button-close");
+
+for (index = 0; index < buttonBuy.length; index++) {
+    button = buttonBuy[index];
+    button.addEventListener("click", function (event) {
+        console.log("click");
+        event.preventDefault();
+        popapBasket.classList.add("popap-basket-show");
+    });
+}
+
+closeBasket.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    popapBasket.classList.remove("popap-basket-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+        evt.preventDefault();
+        if (popapBasket.classList.contains("popap-basket-show")) {
+            popapBasket.classList.remove("popap-basket-show");
+        }
+    }
+});
+
 var linkFeedback = document.querySelector(".contacts .button");
-
 var popapFeedback = document.querySelector(".popap-feedback");
-var closeFeedback = popapFeedback.querySelector(".button-close");
-
+var closeFeedback = popapFeedback.querySelector(".button-close-feedback");
 var formFeedback = popapFeedback.querySelector("form");
 var nameFeedback = popapFeedback.querySelector("[name=username]");
 var emailFeedback = popapFeedback.querySelector("[name=email]");
 var letterFeedback = popapFeedback.querySelector("[name=letter]");
-
 var isStorageSupport = true;
 var storageFeedback = "";
 
 try {
-storageFeedback = localStorage.getItem("emailFeedback");
+    storageFeedback = localStorage.getItem("emailFeedback");
 } catch (err) {
-isStorageSupport = false;
+    isStorageSupport = false;
 }
 
 linkFeedback.addEventListener("click", function(evt) {
@@ -59,9 +83,7 @@ window.addEventListener("keydown", function (evt) {
 });
 
 var linkMap = document.querySelector(".index-map");
-
 var popapMap = document.querySelector(".popap-map");
-
 var closeMap = popapMap.querySelector(".button-close");
 
 linkMap.addEventListener("click", function(evt) {
@@ -79,35 +101,6 @@ window.addEventListener("keydown", function (evt) {
         evt.preventDefault();
         if (popapMap.classList.contains("popap-map-show")) {
             popapMap.classList.remove("popap-map-show");
-        }
-    }
-});
-
-var buttonBuy = document.querySelectorAll(".button-buy"), index, buttom;
-
-var popapBasket = document.querySelector(".popap-basket");
-
-var closeBasket = popapBasket.querySelector(".button-close");
-
-for (index = 0; index < buttonBuy.length; index++) {
-    button = buttonBuy[index];
-    button.addEventListener("click", function (event) {
-        console.log("click");
-        event.preventDefault();
-        popapBasket.classList.add("popap-basket-show");
-    });
-}
-
-closeBasket.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    popapBasket.classList.remove("popap-basket-show");
-});
-
-window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-        evt.preventDefault();
-        if (popapBasket.classList.contains("popap-basket-show")) {
-            popapBasket.classList.remove("popap-basket-show");
         }
     }
 });
@@ -131,52 +124,65 @@ var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+    showSlides(slideIndex += n);
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+    showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
 } 
 
 var slideIndexService = 1;
 showSlidesService(slideIndexService);
 
 function plusSlidesService(n) {
-  showSlidesService(slideIndexService += n);
+    showSlidesService(slideIndexService += n);
 }
 
 function currentSlideService(n) {
-  showSlidesService(slideIndexService = n);
+    showSlidesService(slideIndexService = n);
 }
 
 function showSlidesService(n) {
-  var i;
-  var slidesService = document.getElementsByClassName("mySlidesService");
-  var dotsService = document.getElementsByClassName("dotService");
-  if (n > slidesService.length) {
-    slideIndexService = 1
-}
-  if (n < 1) {
-    slideIndexService = slidesService.length
-}
-  for (i = 0; i < slidesService.length; i++) {
+    var i;
+    var slidesService = document.getElementsByClassName("mySlidesService");
+    var dotsService = document.getElementsByClassName("dotService");
+
+    if (n > slidesService.length) {
+        slideIndexService = 1
+    }
+
+    if (n < 1) {
+        slideIndexService = slidesService.length
+    }
+
+    for (i = 0; i < slidesService.length; i++) {
       slidesService[i].style.display = "none";
-  }
-  slidesService[slideIndexService-1].style.display = "block";
+    }
+
+    slidesService[slideIndexService-1].style.display = "block";
 };
